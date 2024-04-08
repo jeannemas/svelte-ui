d439c1ce1a1b39e156af92d55807fceeea2a119b
Jeanne Mas <jeannemas@jeannemas.me>
2024-04-08 17:26:20 -0400

    Add missing export file

---
+++ src/lib/components/collapsible/index.ts
@@ -0,0 +1,10 @@
+import { Collapsible as CollapsiblePrimitive } from 'bits-ui';
+
+export {
+  default as Content,
+  type Attributes as ContentAttributes,
+  type Events as ContentEvents,
+  type Props as ContentProps,
+} from './Content.svelte';
+
+export const { Root, Trigger } = CollapsiblePrimitive;
